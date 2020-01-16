38798ae31ba1ad761ce56a43b037aa881f7efb15
mountcloud <mountcloud@outlook.com>
2020-01-17 00:31:45 +0800

    init

---
+++ src/main/java/org/mountcloud/springcloud/common/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author zhanghaishan
+ * @version V1.0
+ * org.mountcloud.mvc.common
+ * TODO:
+ * 2020年1月8日.
+ */
+package org.mountcloud.springcloud.common;
---
+++ src/test/java/org/mountcloud/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author zhanghaishan
+ * @version V1.0
+ * org.mountcloud.mvc.common
+ * TODO:
+ * 2019Äê12ÔÂ31ÈÕ.
+ */
+package org.mountcloud;
